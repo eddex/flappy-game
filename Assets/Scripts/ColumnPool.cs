05f79f9ec0f61ab76817cf134c3bece19404345d
eddex <eddex@outlook.com>
2018-09-27 21:50:08 +0200

    added moving columns with object pooling of 5 objects

---
+++ Assets/Scripts/ColumnPool.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class ColumnPool : MonoBehaviour
+{
+    public int ColumnPoolSize = 5;
+    public GameObject ColumnPrefab;
+    public float SpawnRate = 4f;
+    public float ColumnMin = -1f;
+    public float ColumnMax = 1f;
+
+    private GameObject[] columns;
+    private Vector2 objectPoolPosition = new Vector2(-15f, -25f);
+    private float timeSinceLastSpawned;
+    private int currentColumn = 0;
+
+	void Start ()
+    {
+        this.columns = new GameObject[this.ColumnPoolSize];
+        for (int i = 0; i < this.ColumnPoolSize; i++)
+        {
+            this.columns[i] = GameObject.Instantiate(this.ColumnPrefab, this.objectPoolPosition, Quaternion.identity);
+        }
+		
+	}
+	
+	void Update ()
+    {
+        this.timeSinceLastSpawned += Time.deltaTime;
+        if (!GameControl.Instance.GameOver && this.timeSinceLastSpawned > this.SpawnRate)
+        {
+            this.timeSinceLastSpawned = 0;
+            var spawnPositonY = Random.Range(this.ColumnMin, this.ColumnMax);
+            this.columns[this.currentColumn].transform.position = new Vector2(10f, spawnPositonY);
+            this.currentColumn++;
+            if (this.currentColumn >= this.ColumnPoolSize)
+            {
+                this.currentColumn = 0;
+            }
+        }
+	}
+}
